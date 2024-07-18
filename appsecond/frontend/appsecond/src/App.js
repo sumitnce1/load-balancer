82fb0b7b86a1960de61ac49b08dcd7424f96e6ab
SUMIT <sumit.k@cdac.in>
2024-07-18 18:08:51 +0530

    feat: load-balancer

---
+++ appsecond/frontend/appsecond/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+const dataurl = process.env.REACT_APP_API_HOST;
+
+function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch(`${dataurl}/api/data`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data in App Second:", error);
+        setMessage("Error fetching data in App Second");
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="scroll-container" style={{ marginTop: "15vh", color: "green" }}>
+        <h1 id="scroll-text">{message}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
